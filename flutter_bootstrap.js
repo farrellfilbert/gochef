e6b3cf620ccc815d3e34417913e92490fc8ab51b
farrell filbert <farrellfilbert05@gmail.com>
2026-06-22 15:43:38 +0700

    Built web
    Branch: main
    Commit: 74172bca009a3db09d983af847314a3439266abf
    
    package:peanut 6.0.1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1255696722" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4079351550" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
