d5a87ab026767852a781743ce4768f64771a0e85
GoChef Dev <dev@gochef.com>
2026-08-05 15:40:21 +0700

    Update web build with exact login error messages

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "416582794" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "446259054" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
