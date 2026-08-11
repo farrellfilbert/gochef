31e6c9d9ed22040a09d1991639ff0fd893ba863d
GoChef Dev <dev@gochef.com>
2026-08-11 11:19:10 +0700

    Check kitchen owner

--- api/fix_db.php
+++ api/fix_db.php
@@ -1,1 +1,1 @@
-<?php require "db_connect.php"; try { $stmt = $pdo->query("SELECT * FROM cart_items WHERE user_id = 9"); print_r($stmt->fetchAll()); } catch(Exception $e) { echo $e->getMessage(); } ?>
+<?php require "db_connect.php"; try { $stmt = $pdo->query("SELECT * FROM kitchens WHERE id = 7"); print_r($stmt->fetchAll()); } catch(Exception $e) { echo $e->getMessage(); } ?>
