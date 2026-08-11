d7e99d617112a51ca64a4fa81f08eea252178795
GoChef Dev <dev@gochef.com>
2026-08-11 14:41:23 +0700

    Add loading splash screen to web app

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1076319349" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3572426770" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
