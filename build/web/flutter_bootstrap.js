c72936c3da968d88c51241ebe3cce35de35beab3
GoChef Dev <dev@gochef.com>
2026-08-12 22:02:11 +0700

    Fix compile error with TextStyles

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "279079573" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2863098258" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
