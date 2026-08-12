7ee83101fae02d757c77147529697d6109a57087
GoChef Dev <dev@gochef.com>
2026-08-12 22:00:00 +0700

    Move categories under popular meals and add filter

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3900626502" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "279079573" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
