ff36c3be2c094bcda404e073a23ad05d6262b0f1
GoChef Dev <dev@gochef.com>
2026-08-13 12:59:47 +0700

    Fix OrderItemModel import in order_tracking_screen.dart

--- lib/screens/tracking/order_tracking_screen.dart
+++ lib/screens/tracking/order_tracking_screen.dart
@@ -8,6 +8,8 @@ import 'order_review_screen.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'dart:math';
+import 'package:socket_io_client/socket_io_client.dart' as IO;
+import 'package:go_chef_app/models/order_model.dart';
 
 class OrderTrackingScreen extends StatefulWidget {
   final String orderId;
