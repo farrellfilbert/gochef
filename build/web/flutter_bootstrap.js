f11f4702d0c83bd4664e1b411cd2ce1a027d1c85
GoChef Dev <dev@gochef.com>
2026-08-14 22:40:54 +0700

    deploy: high performance full quality build with browser caching

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1652520577" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1568298127" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
