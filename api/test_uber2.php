d09c911972876fef545e0c8cff64f83b1294cbb4
GoChef Dev <dev@gochef.com>
2026-08-29 00:04:35 +0700

    Test multi client ids

--- api/test_uber2.php
+++ api/test_uber2.php
@@ -1,26 +1,29 @@
 <?php
 require_once 'config.php';
 
-$url = 'https://auth.uber.com/oauth/v2/token';
-$postData = http_build_query([
-    'client_id' => UBER_CLIENT_ID,
-    'client_secret' => UBER_CLIENT_SECRET,
-    'grant_type' => 'client_credentials',
-    'scope' => 'eats.deliveries'
-]);
+$client_ids = [
+    'jmjGCSlzbifIXAHT-DihFnCvQmJar4aB',
+    'jmjGCSlzbiflXAHT-DihFnCvQmJar4aB',
+    'jmjGCSIzbifIXAHT-DihFnCvQmJar4aB',
+    'jmjGCSIzbiflXAHT-DihFnCvQmJar4aB'
+];
 
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
-curl_setopt($ch, CURLOPT_HTTPHEADER, [
-    'Content-Type: application/x-www-form-urlencoded'
-]);
+foreach ($client_ids as $cid) {
+    $postData = http_build_query([
+        'client_id' => $cid,
+        'client_secret' => UBER_CLIENT_SECRET,
+        'grant_type' => 'client_credentials',
+        'scope' => 'eats.deliveries'
+    ]);
 
-$response = curl_exec($ch);
-$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-curl_close($ch);
-
-echo "HTTP Code: $httpCode\n";
-echo "Response: $response\n";
+    $ch = curl_init('https://auth.uber.com/oauth/v2/token');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+    $response = curl_exec($ch);
+    $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+    
+    echo "ID: $cid | Code: $httpCode | Resp: $response\n";
+}
 ?>
