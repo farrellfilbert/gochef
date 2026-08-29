975d06d66ee9a52013090f597379ac9da505fa72
GoChef Dev <dev@gochef.com>
2026-08-29 10:20:13 +0700

    Enable error reporting in test_quote.php

--- api/test_quote.php
+++ api/test_quote.php
@@ -1,5 +1,9 @@
 <?php
-require_once 'api/uber_service.php';
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require_once 'uber_service.php';
 
 $quoteResult = UberService::getDeliveryQuote('123 Main St, New York, NY 10001', '456 Elm St, New York, NY 10001');
 
