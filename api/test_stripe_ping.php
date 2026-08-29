38311fcad31650f3eab87d742271d1031b437c9c
GoChef Dev <dev@gochef.com>
2026-08-29 10:47:40 +0700

    IPv4 Stripe ping

--- api/test_stripe_ping.php
+++ api/test_stripe_ping.php
@@ -2,6 +2,7 @@
 $ch = curl_init("https://api.stripe.com/v1/charges");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+curl_setopt($ch, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
 $res = curl_exec($ch);
 if(curl_errno($ch)){
     echo 'Curl error: ' . curl_error($ch);
